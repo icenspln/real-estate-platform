239bdd968199979270b8050a98c5b13c1c36ae0a
icenspln <icenpln@proton.me>
2025-11-20 11:39:09 +0100

    bootstrap endpoint test

--- api/controllers/bootstrapController.js
+++ api/controllers/bootstrapController.js
@@ -14,14 +14,22 @@ module.exports = (User) => async (req, res, next) => {
       },
     });
 
+    if (token !== process.env.ADMIN_TOKEN) {
+      logg("FALSE TOKEN");
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
     if (count > 0) {
       logg("ADMIN EXISTS");
       return res.status(403).json({ success: false, message: "Forbidden" });
     }
 
-    if (token !== process.env.ADMIN_TOKEN) {
-      logg("FALSE TOKEN");
-      return res.status(401).json({ success: false, message: "Unauthorized" });
+    const user = await User.findOne({ where: { email: email } });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User email already in use" });
     }
 
     const hash = await bcrypt.hash(password, 10);
--- api/tests/routes.test.js
+++ api/tests/routes.test.js
@@ -2,6 +2,7 @@
 const request = require("supertest");
 const setupTestDB = require("./setupTestDB");
 const createApp = require("../app");
+require("dotenv").config();
 
 let sequelize, models, app;
 
@@ -15,7 +16,7 @@ afterAll(() => {
   sequelize.close();
 });
 
-describe("testing API end points", () => {
+describe("testing API /auth", () => {
   test("should signup user", async () => {
     const res = await request(app).post("/auth/signup").send({
       firstName: "jsonmomoa",
@@ -59,5 +60,49 @@ describe("testing API end points", () => {
     });
     expect(res.statusCode).toEqual(200);
     expect(res.body.token).toBeTruthy();
+  });
+});
+
+describe("testing API /bootstrap", () => {
+  test("should fail creating the first admin with invalid token", async () => {
+    const res = await request(app)
+      .post(`/bootstrap/admin?token=${process.env.ADMIN_TOKEN}1`)
+      .send({
+        firstName: "jsonmomoa",
+        lastName: "somelastname",
+        username: "michael jackson",
+        email: "newadminlikeemail@privatelife.lol",
+        role: "admin",
+        password: "guest123",
+      });
+    expect(res.statusCode).toEqual(401);
+  });
+
+  test("should create the first admin", async () => {
+    const res = await request(app)
+      .post(`/bootstrap/admin?token=${process.env.ADMIN_TOKEN}`)
+      .send({
+        firstName: "jsonmomoa",
+        lastName: "somelastname",
+        username: "michael jackson",
+        email: "newadminlikeemail@privatelife.lol",
+        role: "admin",
+        password: "guest123",
+      });
+    expect(res.statusCode).toEqual(201);
+  });
+
+  test("should fail creating the second admin", async () => {
+    const res = await request(app)
+      .post(`/bootstrap/admin?token=${process.env.ADMIN_TOKEN}`)
+      .send({
+        firstName: "jsonmomoa",
+        lastName: "somelastname",
+        username: "michael jackson",
+        email: "sendmespamplease91@homelone.me",
+        role: "admin",
+        password: "guest123",
+      });
+    expect(res.statusCode).toEqual(403);
   });
 });
